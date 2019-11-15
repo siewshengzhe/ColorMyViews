a85d581b14ed66b027ffeb7a1021cdec6446ca3c
shengzhe <shengzhe987#gmail.com>
2019-11-15 11:51:37 +0800

    done

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,15 +17,22 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun setListeners(){
+     private fun setListeners(){
 
 
         val clickableViews: List<View> =
             listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text, rootConstraintLayout)
+                box_four_text, box_five_text, rootConstraintLayout,
+                red_button, green_button, yellow_button
+            )
 
         for (item in clickableViews){
             item.setOnClickListener(){makeColored(it)}
+
+
+            val redbutton = findViewById<TextView>(R.id.red_button)
+            val greenButton = findViewById<TextView>(R.id.green_button)
+            val yellowButton = findViewById<TextView>(R.id.yellow_button)
         }
     }
 
@@ -41,7 +49,12 @@ class MainActivity : AppCompatActivity() {
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
 
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
+
         }
     }
 }
